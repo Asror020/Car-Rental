256f11fad7ec0ce41438698299a529483a8eceee
Asrorjon Kodirov <asrora020@gmail.com>
2023-12-09 00:28:58 +0500

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,78 @@
+﻿using Car_Rental.Models;
+using Context;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Car_Rental.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public UsersController(ApplicationDbContext context) 
+        {
+            this.context = context;
+        }
+
+        [HttpGet("signin")]
+        public ActionResult SignIn(string username, string password)
+        {
+            var user = context.Users.FirstOrDefault(x => x.Username == username && x.Password == password);
+            if (user == null)
+                return BadRequest("User not found");
+            return Ok(true);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAll()
+        {
+            return context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User?> Get(int id)
+        {
+            var user = context.Users.FirstOrDefault(x => x.Id == id);
+            return user == null ? BadRequest() : Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult<User> Post([FromBody] User user)
+        {
+            var createdUser = context.Users.Add(user);
+            context.SaveChanges();
+
+            return user == null ? BadRequest() : Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<User> Update(int id, [FromBody] User user)
+        {
+            var userToFind = context.Users.FirstOrDefault(x => x.Id == id);
+
+            if (userToFind == null)
+                return BadRequest();
+
+            user.Id = id;
+            var result = context.Users.Update(user);
+            context.SaveChanges();
+
+            return result == null ? BadRequest() : Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<bool> Delete(int id)
+        {
+            var userToRemove = context.Users.FirstOrDefault(x => x.Id == id);
+            if(userToRemove == null)
+                return BadRequest();
+
+            context.Users.Remove(userToRemove);
+            context.SaveChanges();
+
+            return Ok(true);
+        }
+    }
+}
---
+++ Migrations/20231208191323_noviy1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRental.Migrations
+{
+    /// <inheritdoc />
+    public partial class noviy1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
